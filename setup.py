1679d078efe8867185b1104e4b474a4967253818
f-tominaga <tommy.tommy.walker@gmail.com>
2025-12-31 06:05:25 +0000

    add color2grayscale node

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'color2grayscale_node = chinorobo_ros2_opencv_03.color2grayscale:main',
         ],
     },
 )
